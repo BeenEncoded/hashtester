c646038bb0c60b3c9cb70fd22638e465f8669f90
Jonathan Whitlock <beenencoded@outlook.com>
2019-10-09 14:46:17 -0400

    updated algorithm to not take up all the memory; added a progress bar;

--- hashtest.py
+++ hashtest.py
@@ -1,13 +1,14 @@
-import sys, hashlib
+import sys, hashlib, os, threading, dataclasses
 
 from PyQt5.QtWidgets import *
 from enum import IntEnum
 from pathlib import Path
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtSignal, QObject
 from PyQt5.QtGui import QFont
 
 global_font = QFont("monospaced", 10)
 result_font = QFont("monospaced", 14)
+HASH_BLOCKSIZE = ((2**10) * 5) #10 Kb blocksize
 
 class hash_function_t(IntEnum):
     MD5 = 0
@@ -16,29 +17,77 @@ class hash_function_t(IntEnum):
     SHA384 = 3
     SHA512 = 4
 
-# Generates a tuple of hashes for the specified file.
-# The tuple is such that you should be able to reference its members
-# with hash_function_t.  Example:  hashes[hash_function_t.MD5] would be the md5
-# hash.
-def generate_hash(target):
-    if(Path(target).is_file() == False):
-        return None
-    file = open(target, "rb").read()
-    return (hashlib.md5(file).hexdigest(), \
-            hashlib.sha1(file).hexdigest(), \
-            hashlib.sha256(file).hexdigest(), \
-            hashlib.sha384(file).hexdigest(), \
-            hashlib.sha512(file).hexdigest())
+@dataclasses.dataclass
+class ProcessStatus:
+    '''
+    Stores a process' status
+    '''
+    percent: float = 0
+    message: str = ""
+
+class HashThread(threading.Thread):
+    class guicom(QObject):
+        updatestatus = pyqtSignal(ProcessStatus)
+        finishedprocess = pyqtSignal()
+
+    def __init__(self, target, blocksize=HASH_BLOCKSIZE):
+        super(HashThread, self).__init__()
+        self.status = ProcessStatus()
+        self.target = target
+        self.blocksize = blocksize
+        self.hashes = None
+
+        self.com = HashThread.guicom()
+
+    # Generates a tuple of hashes for the specified file.
+    # The tuple is such that you should be able to reference its members
+    # with hash_function_t.  Example:  hashes[hash_function_t.MD5] would be the md5
+    # hash.
+    def run(self):
+        if not os.path.isfile(self.target):
+            return None
+        hashes = [
+            hashlib.md5(),
+            hashlib.sha1(),
+            hashlib.sha256(),
+            hashlib.sha384(),
+            hashlib.sha512()
+        ]
+        file_size = os.stat(self.target).st_size
+        file_read = 0
+        self.status.message = "Hashing..."
+        self.status.percent = 0
+        self._setstatus(self.status)
+        with open(self.target, 'rb') as file:
+            while True:
+                block = file.read(self.blocksize)
+                file_read += len(block)
+                self.status.percent = ((file_read * 100) / file_size)
+                self._setstatus(self.status)
+                if len(block) == 0:
+                    break
+                for h in hashes:
+                    h.update(block)
+        self.hashes = [h.hexdigest() for h in hashes]
+        self._finished()
+
+    def _setstatus(self, status):
+        self.com.updatestatus.emit(status)
+    
+    def _finished(self):
+        self.com.finishedprocess.emit()
 
 class input_widget(QWidget):
     def __init__(self, parent):
         super(input_widget, self).__init__(parent)
         self.setLayout(self._input_layout())
+        self._connect_slots()
     
     def _input_layout(self):
         self.hash_textbox = QLineEdit()
         self.test_button = QPushButton("Test a File Against this Hash")
         self.result_label = QLabel("No Match!")
+        self.progress_bar = QProgressBar()
 
         self.hash_labels = []
         for e in hash_function_t:
@@ -50,12 +99,12 @@ class input_widget(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.hash_textbox)
         layout.addWidget(self.test_button)
+        layout.addWidget(self.progress_bar)
         layout.addWidget(self.result_label)
 
         for e in hash_function_t:
             layout.addLayout(self._label_hash(e.name, self.hash_labels[e]))
-
-        self.test_button.clicked.connect(self._test)
+        
         return layout
 
     def _label_hash(self, hashname, label):
@@ -66,22 +115,37 @@ class input_widget(QWidget):
         l.addWidget(label)
         return l
     
+    @pyqtSlot(ProcessStatus)
+    def _statusUpdate(self, status):
+        self.progress_bar.setValue(status.percent)
+
+    def run_hashes(self, target, blocksize=HASH_BLOCKSIZE):
+        self.hash_thread = HashThread(target, blocksize)
+        self.hash_thread.com.updatestatus.connect(self._statusUpdate)
+        self.hash_thread.com.finishedprocess.connect(self._hashingFinished)
+        self.hash_thread.start()
+
+    @pyqtSlot()
+    def _hashingFinished(self):
+        self.hash_thread.join()
+        if(self.hash_thread.hashes is not None):
+            self.result_label.setText("No Match.")
+            for h in hash_function_t:
+                if(self.hash_thread.hashes[h] == self.hash_textbox.text()):
+                    self.result_label.setText("MATCH!")
+                self.hash_labels[h].setText(self.hash_thread.hashes[h])
+        else:
+            self.result_label.setText("Canceled.")
+
+    @pyqtSlot()
     def _test(self):
         self.result_label.setText("Please wait, reading file...")
         target = QFileDialog.getOpenFileName()[0]
-        hashes = generate_hash(target)
-        if(hashes is not None):
-            self.result_label.setText("No Match.")
-            for h in hash_function_t:
-                if(hashes[h] == self.hash_textbox.text()):
-                    self.result_label.setText("MATCH!")
-                self.hash_labels[h].setText(hashes[h])
-        else:
-            self.result_label.setText("Canceled.")
-            
+        self.run_hashes(target)
     
     def _connect_slots(self):
-        pass
+        self.test_button.clicked.connect(self._test)
+        
 
 class main_window(QMainWindow):
     def __init__(self, parent):
